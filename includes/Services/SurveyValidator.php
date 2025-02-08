d3450f5b3f6d4b8364ecfdd1b75d29dac6211525
amir-canteetu <amircanteetu@gmail.com>
2025-02-08 16:02:22 -0700

    refactoring

--- includes/Services/SurveyValidator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class SurveyValidator {
-    public function validate(array $data): bool {
-        $schema = json_decode(file_get_contents('config/survey-schema.json'));
-        return (new Validator())->validate($data, $schema);
-    }
-}
-
