99f494860912771c88f32a79a327c158259a395c
amir-canteetu <amircanteetu@gmail.com>
2025-02-15 08:49:18 -0700

    removed Vite

--- assets/admin/js/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import RuleBuilder from "@/RuleBuilder";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("cdwc-rule-builder-root");
-  if (container) {
-    createRoot(container).render(<RuleBuilder />);
-  }
-});
